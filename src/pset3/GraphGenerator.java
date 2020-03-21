e67d231d57006c85a9b060098b9fe818f845e5bc
fle734449 <36311780+fle734449@users.noreply.github.com>
2020-03-21 13:10:21 -0500

    Update GraphGenerator.java

--- src/pset3/GraphGenerator.java
+++ src/pset3/GraphGenerator.java
@@ -20,6 +20,17 @@ public class GraphGenerator {
 				cfg.addNode(position, m, jc);
 				Instruction inst = ih.getInstruction();
 				// your code goes here
+				
+				if(inst instanceof ReturnInstruction) {
+					cfg.addEdge(position, -1, m, jc);
+				} else if(inst instanceof BranchInstruction) {
+					BranchInstruction branchInstruction = (BranchInstruction) inst;
+					int nextPosition = branchInstruction.getTarget().getPosition();
+					cfg.addEdge(position, m, jc, nextPosition, m, jc);	
+				} else {
+					int nextPosition = ih.getNext().getPosition();
+					cfg.addEdge(position, nextPosition, m, jc);
+				}
 			}
 		}
 		return cfg;
@@ -31,7 +42,8 @@ public class GraphGenerator {
 
 	public static void main(String[] a) throws ClassNotFoundException {
 		GraphGenerator gg = new GraphGenerator();
-		gg.createCFG("pset3.C"); // example invocation of createCFG
+		CFG cfg = gg.createCFG("pset3.C"); // example invocation of createCFG
+		System.out.println(cfg.toString());
 		gg.createCFGWithMethodInvocation("pset3.D"); // example invocation of createCFGWithMethodInovcation
 	}
 }
